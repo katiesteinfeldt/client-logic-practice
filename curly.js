8adaec34592ec7cb8a8bc6e335b77bfaaae24449
Katie Mangan <katie.mangan1@gmail.com>
2019-04-18 14:03:44 -0500

    remove punctuation from array of strings inputted

--- curly.js
+++ curly.js
@@ -4,6 +4,11 @@ handleSynoym = (text, synomym) => {
     console.log('running handle synonym');
     const textArray = text.split(' ');
     console.log(textArray);
+    const keyWords = [];
+    textArray.forEach(word => {
+        word = word.replace(/[^a-zA-Z0-9]/g, '');
+        console.log(word);
+    })
 }
 
 
