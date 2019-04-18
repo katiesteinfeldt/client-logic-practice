3b368706d266e650845d324775c2843c00447ce2
Katie Mangan <katie.mangan1@gmail.com>
2019-04-18 13:59:39 -0500

    inital commit

---
+++ curly.js
@@ -0,0 +1,12 @@
+const curlyHair = ['curly', 'curly hair'];
+
+handleSynoym = (text, synomym) => {
+    console.log('running handle synonym');
+}
+
+
+
+
+
+
+handleSynoym('Do you cut curly hair?', curlyHair);
