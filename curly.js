704e388397ccbdef19d0ee5848fd88d6b052a157
Katie Mangan <katie.mangan1@gmail.com>
2019-04-18 14:02:05 -0500

    console log separate strings in array

--- curly.js
+++ curly.js
@@ -2,6 +2,8 @@ const curlyHair = ['curly', 'curly hair'];
 
 handleSynoym = (text, synomym) => {
     console.log('running handle synonym');
+    const textArray = text.split(' ');
+    console.log(textArray);
 }
 
 
